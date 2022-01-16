0cd3a62420989ed691d44d1d4775d87b96a4c848
Moopz-Dev <pongzthor@gmail.com>
2022-01-16 18:33:43 +0700

    initializes express, add route skeleton, error handling, and server listener

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-const { sequelize } = require("./models");
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
-console.log("run index successfully");
+// initialize express
+const app = express();
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(cors());
 
-sequelize.sync();
+// routes
+
+//error handling
+app.use((req, res) => {
+	res.status(404).json({ message: "resource not found on this server" });
+});
+
+app.use((err, req, res, next) => {
+	console.log(err);
+	let code = 500;
+	if (err.name === "JsonWebTokenError" || err.name === "TokenExpiredError") {
+		code = 401;
+	}
+	if (process.env.NODE_ENV === "development") {
+		res.status(code).json({ error: err.message });
+	} else {
+		res.status(code).json({ message: " Your mom is sad." });
+	}
+});
+
+//server hosting
+app.listen(process.env.PORT || 8999, () =>
+	console.log("server listening on port " + process.env.PORT || 8999)
+);
