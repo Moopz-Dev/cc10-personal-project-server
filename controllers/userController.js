1de36b3e0ff399a28deab4be502ce7cef752a85e
Moopz-Dev <pongzthor@gmail.com>
2022-01-17 21:37:14 +0700

    create endpoint skeletons for user

---
+++ controllers/userController.js
@@ -0,0 +1,48 @@
+exports.addCartItem = async (req, res, next) => {
+	try {
+	} catch (error) {
+		next(error);
+	}
+};
+
+exports.getCartItem = async (req, res, next) => {
+	try {
+	} catch (error) {
+		next(error);
+	}
+};
+
+exports.removeCartItem = async (req, res, next) => {
+	try {
+	} catch (error) {
+		next(error);
+	}
+};
+
+exports.changeAddress = async (req, res, next) => {
+	try {
+	} catch (error) {
+		next(error);
+	}
+};
+
+exports.createOrder = async (req, res, next) => {
+	try {
+	} catch (error) {
+		next(error);
+	}
+};
+
+exports.getOrders = async (req, res, next) => {
+	try {
+	} catch (error) {
+		next(error);
+	}
+};
+
+exports.useCoupon = async (req, res, next) => {
+	try {
+	} catch (error) {
+		next(error);
+	}
+};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const userController = require("../controllers/userController");
 const authController = require("../controllers/authController");
-
+const {
+	authenticateUser,
+	authenticateAdmin,
+} = require("../middlewares/authenticate");
 const router = express.Router();
 
+//register & login
 router.post("/register", authController.register);
 router.post("/login", authController.login);
+
+//user endpoints
+router.post("/user/cart", userController.addCartItem);
+router.get("/user/cart", userController.getCartItem);
+router.delete("/user/cart", userController.removeCartItem);
+
+router.post("/user/address", userController.changeAddress);
+router.post("/user/order", userController.createOrder);
+router.get("/user/orders", userController.getOrders);
+router.post("/user/order/coupon", userController.useCoupon);
 
 module.exports = router;
