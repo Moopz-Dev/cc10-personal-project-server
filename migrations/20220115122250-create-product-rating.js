a2759c1ebd958ce9de73491053668f2f95f3ff45
Moopz-Dev <pongzthor@gmail.com>
2022-01-28 07:52:18 +0700

    create productRating model,migration and seed

---
+++ migrations/20220115122250-create-product-rating.js
@@ -0,0 +1,38 @@
+"use strict";
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable("ProductRatings", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			productId: {
+				type: Sequelize.DataTypes.INTEGER,
+				references: {
+					model: {
+						tableName: "products",
+					},
+					key: "id",
+				},
+				allowNull: false,
+			},
+			rating: {
+				type: Sequelize.DataTypes.INTEGER,
+				allowNull: false,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable("ProductRatings");
+	},
+};
---
+++ models/productRating.js
@@ -0,0 +1,38 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+	class ProductRating extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+			ProductRating.belongsTo(models.Product);
+		}
+	}
+	ProductRating.init(
+		{
+			rating: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+			productId: {
+				type: DataTypes.INTEGER,
+				references: {
+					model: {
+						tableName: "products",
+					},
+					key: "id",
+				},
+				allowNull: false,
+			},
+		},
+		{
+			sequelize,
+			modelName: "ProductRating",
+		}
+	);
+	return ProductRating;
+};
